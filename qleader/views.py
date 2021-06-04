f5707b9d28a65daf35d2a3104a7ed96d06000357
Petri Riihikallio <petri.riihikallio@metis.fi>
2021-06-04 21:48:33 +0300

    Create a home view

--- qleader/views.py
+++ qleader/views.py
@@ -27,6 +27,7 @@ def home(request):
 
     if request.method == 'GET':
         results = QResult.objects.all().order_by('created')
+        # Here we can filter the list before displaying
         return Response(
             {'results': results.values()},
             template_name='home.html'
