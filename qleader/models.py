a955c33f77ae709f1e86a047255f14a5da228119
Matias Tamsi <55825941+matiastamsi@users.noreply.github.com>
2021-06-04 15:26:39 +0300

    Fixed lint error

--- qleader/models.py
+++ qleader/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from jsonfield import JSONField
 # import tequila as tq
 
+
 class QResult(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     result = JSONField()   # Default parameters for the time!!!
