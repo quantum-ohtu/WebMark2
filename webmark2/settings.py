a605394f3bbd5b447272e173a506746c9031d494
Matias Tamsi <55825941+matiastamsi@users.noreply.github.com>
2021-06-04 21:52:47 +0300

    Root directory is now defined in webmark2/settings.py (takes from os). Also, secret key is now passed same way.

--- webmark2/settings.py
+++ webmark2/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "secret"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,6 +121,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+ROOT_DIR = os.getenv("ROOT_DIR", '')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
