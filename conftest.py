1b7b291d8b9700f99ac229d3c35c28af7573112b
Jami Heljomaa <mail@jami.one>
2021-06-25 01:24:42 +0300

    modified conftest.py to address the source that helped solve an issue

--- conftest.py
+++ conftest.py
@@ -2,19 +2,20 @@ import os
 import django
 from django.conf import settings
 
+# Note to devs: this file with its commentary is modified from the instructions published here:
+# http://engineroom.trackmaven.com/blog/using-pytest-with-django/
+# Check the link if you are confused about an issue regarding the testing config.
+
 # We manually designate which settings we will be using in an environment variable
 # This is similar to what occurs in the `manage.py`
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webmark2.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webmark2.settings")
 
 
 # `pytest` automatically calls this function once when tests are run.
 def pytest_configure():
     settings.DEBUG = True
-    # If you have any test specific settings, you can declare them here,
-    # e.g.
+    # Test specific settings can be declared here, e.g.
     # settings.PASSWORD_HASHERS = (
     #     'django.contrib.auth.hashers.MD5PasswordHasher',
     # )
     django.setup()
-    # Note: In Django =< 1.6 you'll need to run this instead
-    # settings.configure()
