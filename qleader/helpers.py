aabda87b245738c402db7f6b2bad6d5747dbad66
Henri0088 <Henri.Sundquist@rokki.net>
2021-07-01 14:28:05 +0300

    Modified helpers.py to be more modular, easier for testing and future parsing of ansatzes, molecules etc. Also added some initial tests and example outputs of VQE runs using BFGS and Nelder-Mead for testing purposes.

--- qleader/helpers.py
+++ qleader/helpers.py
@@ -7,24 +7,40 @@ def create_qbatch(data):
     qbatch_dict = {key: data[key] for key in keys}
     return QBatch(**qbatch_dict)
 
+def get_variables(data, i):
+    return data["variables"][i]
+
+def get_hamiltonian(data, i):
+    return data["hamiltonian"][i]
+
+def get_ansatz(data, i):
+    return data["ansatz"][i]
+
+def get_molecule(data, i):
+    return data["molecule"][i]
+
+def get_distance(data, i):
+    return data["distances"][i]
+
+def get_history(data, i):
+    history_dict = {
+        str(key): str(val)
+        for key, val in ast.literal_eval(data["histories"][i]).items()
+    }
+    return history_dict
+
+def get_scipy_results(data, i):
+    return ast.literal_eval(data["scipy_results"][i])
 
 def create_qresults(data, qbatch):
     sep_data = [{"energy": e} for e in data["energies"]]
-
-    # TODO parse variables, ansatz and molecule more thoroughly.
     for i, entry in enumerate(sep_data):
-        entry["variables"] = data["variables"][i]
-        entry["hamiltonian"] = data["hamiltonian"][i]
-        entry["ansatz"] = data["ansatz"][i]
-        entry["molecule"] = data["molecule"][i]
-        entry["distance"] = data["distances"][i]
-        history_dict = {
-            str(key): str(val)
-            for key, val in ast.literal_eval(data["histories"][i]).items()
-        }
-        entry.update(history_dict)
-        scipy_results_dict = ast.literal_eval(data["scipy_results"][i])
-        entry.update(scipy_results_dict)
-
+        entry["variables"] = get_variables(data, i)
+        entry["hamiltonian"] = get_hamiltonian(data, i)
+        entry["ansatz"] = get_ansatz(data, i)
+        entry["molecule"] = get_molecule(data, i)
+        entry["distance"] = get_distance(data, i)
+        entry.update(get_history(data, i))
+        entry.update(get_scipy_results(data, i))
     QResults = [QResult(batch=qbatch, **entry) for entry in sep_data]
     return QResults
--- qleader/tests/test_helpers.py
+++ qleader/tests/test_helpers.py
@@ -1,15 +1,66 @@
-# import ast
-from qleader.helpers import create_qbatch
+from qleader.models import QBatch
+import qleader.helpers as helpers
+import os, sys, ast
 
 # from qleader.models import QBatch, QResult
 import pytest
 
+# example files are outputs generated with the basic VQE example
+# here we simply test that the information is extracted correctly.
+nelder_mead = ast.literal_eval(open(os.path.join(sys.path[0], "example_nelder_mead.txt"), "r").read())
+bfgs = ast.literal_eval(open(os.path.join(sys.path[0], "example_BFGS.txt"), "r").read())
 
-def test_totalDummy():
-    assert 2 == 2
+def test_get_history_returns_dict():
+    history_dict = helpers.get_history(nelder_mead, 0)
+    assert type(history_dict) is dict
 
+def test_get_history_returns_energies():
+    history_dict = helpers.get_history(nelder_mead, 1)
+    energy_list = ast.literal_eval(history_dict['energies'])
+    assert energy_list[0] == -1.0661086491853138
+
+def test_get_hamiltonian():
+    hamiltonian = helpers.get_hamiltonian(nelder_mead, 0)
+    assert "+0.3798+0.2139Z(0)+0.2139Z(1)-0.3691Z(2)-0.3691Z(3)" in hamiltonian
+
+def test_scipy_results_returns_dict():
+    scipy_dict = helpers.get_scipy_results(nelder_mead, 0)
+    assert type(scipy_dict) is dict
+
+def test_scipy_results_for_nelder_mead():
+    scipy_dict = helpers.get_scipy_results(nelder_mead, 0)
+    fields = ['final_simplex', 'fun', 'message', 'nfev', 'nit', 'status', 'success', 'x']
+    for field in fields:
+        assert field in scipy_dict.keys()
+
+def test_scipy_results_for_bfgs():
+    scipy_dict = helpers.get_scipy_results(bfgs, 0)
+    fields = ['fun', 'hess_inv', 'jac', 'message', 'nfev', 'nit', 'njev', 'status', 'success', 'x']
+    for field in fields:
+        assert field in scipy_dict.keys()
+
+def test_qbatch_creation_nelder_mead():
+    qbatch = helpers.create_qbatch(nelder_mead)
+    assert type(qbatch) is QBatch
+    assert qbatch.optimizer == "Nelder-Mead"
+
+def test_qbatch_creation_bfgs():
+    qbatch = helpers.create_qbatch(bfgs)
+    assert qbatch.optimizer == "BFGS"
+
+def test_qresult_creation_nelder_mead():
+    qbatch = helpers.create_qbatch(nelder_mead)
+    qresults = helpers.create_qresults(nelder_mead, qbatch)
+    assert len(qresults) == 4
+    assert qresults[0].batch is qbatch
+
+def test_qresult_creation_bfgs():
+    qbatch = helpers.create_qbatch(bfgs)
+    qresults = helpers.create_qresults(bfgs, qbatch)
+    assert len(qresults) == 4
+    assert qresults[1].batch is qbatch
 
 class TestClass:
     def test_errorOnInvalidInput(self):
         with pytest.raises(TypeError):
-            create_qbatch("nothing important")
+            helpers.create_qbatch("nothing important")
