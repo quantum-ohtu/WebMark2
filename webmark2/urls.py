55a95decc7a6ce22ff039eafb20b4c6cc2c1361a
Matias Tamsi <55825941+matiastamsi@users.noreply.github.com>
2021-07-02 14:08:07 +0300

    Changed batch_id -> run_id

--- webmark2/urls.py
+++ webmark2/urls.py
@@ -19,7 +19,7 @@ from qleader.views import result_list, detail, home
 # from webmark2.settings import ROOT_DIR
 
 urlpatterns = [
-    path('api/<int:batch_id>/', detail),
+    path('api/<int:run_id>/', detail),
     path('api/', result_list),
     # path('admin/', admin.site.urls),
     path('', home),
