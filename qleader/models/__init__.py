d3686bd638139e9fa15e95df9335b0bdb91c903d
Jami Heljomaa <mail@jami.one>
2021-07-07 17:34:53 +0300

    cut down requirements.txt + style check

--- qleader/models/__init__.py
+++ qleader/models/__init__.py
@@ -1,3 +1,4 @@
 from .result import Result  # noqa: F401
 from .run import Run  # noqa: F401
-from .run_scipy import RunScipy, RunScipyNelderMead, RunScipyBFGS, RunScipyLBFGSB, RunScipyCOBYLA  # noqa: F401
+from .run_scipy import RunScipy, RunScipyNelderMead, RunScipyBFGS  # noqa: F401
+from .run_scipy import RunScipyLBFGSB, RunScipyCOBYLA  # noqa: F401
--- qleader/models/run_scipy.py
+++ qleader/models/run_scipy.py
@@ -4,7 +4,8 @@ from django.db.models.fields import CharField, TextField, FloatField, SmallInteg
 from qleader.models.run import Run
 from .result import Result
 
-# An abstract class for scipy based 
+
+# An abstract class for scipy based
 class RunScipy(Run):
     distance = FloatField(default=None)
     energy = FloatField(default=None)
@@ -30,13 +31,17 @@ class RunScipy(Run):
     class Meta:
         abstract = True
 
+
 # "Nelder-Mead" optimizer
 class RunScipyNelderMead(RunScipy):
-    result = models.ForeignKey(Result, related_name='runs_nelder_mead', on_delete=CASCADE)  # Must be here!
+    result = models.ForeignKey(
+        Result, related_name='runs_nelder_mead', on_delete=CASCADE
+        )  # Must be here!
     hessians = TextField(default="", blank=True)
     final_simplex = TextField(default="")
     nfev = SmallIntegerField(default=None, blank=True, null=True)
     nit = SmallIntegerField(default=None)
+
 
 # "BFGS" optimizer
 class RunScipyBFGS(RunScipy):
@@ -48,6 +53,7 @@ class RunScipyBFGS(RunScipy):
     nit = SmallIntegerField(default=None)
     njev = SmallIntegerField(default=None, blank=True, null=True)
 
+
 # "L-BFGS-B" optimizer !!! Could be the same class than BFGS at the moment ^ !!!
 class RunScipyLBFGSB(RunScipy):
     result = models.ForeignKey(Result, related_name='runs_lbfgsb', on_delete=CASCADE)  # Must be here!
@@ -57,6 +63,7 @@ class RunScipyLBFGSB(RunScipy):
     nfev = SmallIntegerField(default=None, blank=True, null=True)
     nit = SmallIntegerField(default=None)
     njev = SmallIntegerField(default=None, blank=True, null=True)
+
 
 # "COBYLA" optimizer
 class RunScipyCOBYLA(RunScipy):
