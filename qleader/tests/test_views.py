be6df2ece46bad430198b48d3179a4a759980758
Matias Tamsi <55825941+matiastamsi@users.noreply.github.com>
2021-07-16 21:13:41 +0300

    Refactored test data sending to own file because multiple classes needs it.

--- qleader/tests/test_views.py
+++ qleader/tests/test_views.py
@@ -1,27 +1,12 @@
-import os
-import sys
-import ast
 import json
-
 from rest_framework import status
 from rest_framework.test import APITransactionTestCase
-
-
-def create_test_data_from_example(path):
-    data = ast.literal_eval(
-        open(os.path.join(sys.path[0], path), "r").read()
-    )
-    return json.dumps(data)
+from qleader.tests.post_data import post_data, create_test_data_from_example
 
 
 nelder_mead = create_test_data_from_example("example_NELDER_MEAD.txt")
 bfgs = create_test_data_from_example("example_BFGS.txt")
 nesterov = create_test_data_from_example("example_NESTEROV.txt")
-
-
-def post_data(self, data):
-    response = self.client.post("/api/", data=data, format='json')
-    return response
 
 
 class ViewsTests(APITransactionTestCase):
