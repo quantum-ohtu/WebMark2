21f9abe73dfb250698eb46e3cd730f34177ff12a
Matias Tamsi <55825941+matiastamsi@users.noreply.github.com>
2021-07-16 19:27:01 +0300

    Tests for views.py

--- qleader/tests/test_initializers.py
+++ qleader/tests/test_initializers.py
@@ -9,7 +9,7 @@ import qleader.initializers as initializers
 # example files are outputs generated with the basic VQE example
 # here we simply test that the information is extracted correctly.
 nelder_mead = ast.literal_eval(
-    open(os.path.join(sys.path[0], "example_nelder_mead.txt"), "r").read()
+    open(os.path.join(sys.path[0], "example_NELDER_MEAD.txt"), "r").read()
 )
 bfgs = ast.literal_eval(open(os.path.join(sys.path[0], "example_BFGS.txt"), "r").read())
 
@@ -22,12 +22,12 @@ def test_get_history_returns_dict():
 def test_get_history_returns_energies():
     history_dict = initializers.get_history(nelder_mead, 1)
     energy_list = ast.literal_eval(history_dict["energies"])
-    assert energy_list[0] == -1.0661086491853138
+    assert energy_list[0] == 0.16417501091932965
 
 
 def test_get_hamiltonian():
     hamiltonian = initializers.get_hamiltonian(nelder_mead, 0)
-    assert "+0.3798+0.2139Z(0)+0.2139Z(1)-0.3691Z(2)-0.3691Z(3)" in hamiltonian
+    assert "+5.0607+0.3008Z(0)+0.3008Z(0)Z(1)-0.7265Z(2)" in hamiltonian
 
 
 def test_scipy_results_returns_dict():
--- qleader/tests/test_views.py
+++ qleader/tests/test_views.py
@@ -1,1 +1,91 @@
-# NEEDS TESTS
+import os
+import sys
+import ast
+import json
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.response import Response
+
+
+def create_test_data_from_example(path):
+    data = ast.literal_eval(
+        open(os.path.join(sys.path[0], path), "r").read()
+    )
+    return json.dumps(data)
+
+nelder_mead = create_test_data_from_example("example_NELDER_MEAD.txt")
+bfgs = create_test_data_from_example("example_BFGS.txt")
+nesterov = create_test_data_from_example("example_NESTEROV.txt")
+
+def post_data(self, data):
+    response = self.client.post("/api/", data=data, format='json')
+    return response
+
+class ViewsTests(APITestCase):
+    def test_result_list_GET(self):
+        response = self.client.get("/api/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_result_list_POST_valid_call(self):
+        response = post_data(self, nelder_mead)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_result_list_POST_invalid_call(self):
+        response = post_data(self, json.dumps([]))
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_home_GET(self):
+        response = self.client.get("")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(response.data["runs"]) == 0)
+        post_data(self, nelder_mead)
+        response = self.client.get("")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(response.data["runs"]) == 1)
+        self.assertEqual(response.data["runs"][0]["optimizer"], "NELDER-MEAD")
+
+    def test_detail_GET(self):
+        post_data(self, nelder_mead)
+        response = self.client.get("/api/1/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_leaderboard_GET(self):
+        response = self.client.get("/leaderboard/")
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+    def test_invoke_leaderboard_closest_minimum(self):
+        post_data(self, nelder_mead)
+        post_data(self, bfgs)
+        post_data(self, nesterov)
+        response = self.client.get("/leaderboard/closest_minimum/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(response.data["results"]) == 3)
+        self.assertEqual(response.data["criterion"], "closest_minimum")
+        self.assertEqual(response.data["results"][0].get_optimizer(), "NESTEROV")
+        self.assertEqual(response.data["results"][1].get_optimizer(), "BFGS")
+        self.assertEqual(response.data["results"][2].get_optimizer(), "NELDER-MEAD")
+
+    def test_invoke_leaderboard_smallest_variance(self):
+        post_data(self, nelder_mead)
+        post_data(self, bfgs)
+        post_data(self, nesterov)
+        response = self.client.get("/leaderboard/smallest_variance/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(response.data["results"]) == 3)
+        self.assertEqual(response.data["criterion"], "smallest_variance")
+        self.assertEqual(response.data["results"][0].get_optimizer(), "NELDER-MEAD")
+        self.assertEqual(response.data["results"][1].get_optimizer(), "BFGS")
+        self.assertEqual(response.data["results"][2].get_optimizer(), "NESTEROV")
+
+    def test_invoke_leaderboard_min_energy(self):
+        post_data(self, nelder_mead)
+        post_data(self, bfgs)
+        post_data(self, nesterov)
+        response = self.client.get("/leaderboard/min_energy/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(response.data["results"]) == 3)
+        self.assertEqual(response.data["criterion"], "min_energy")
+        self.assertEqual(response.data["results"][0].get_optimizer(), "NESTEROV")
+        self.assertEqual(response.data["results"][1].get_optimizer(), "BFGS")
+        self.assertEqual(response.data["results"][2].get_optimizer(), "NELDER-MEAD")
