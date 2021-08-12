6a3c3e44ab1b9377452818f7a1d02580823f422c
Henri Sundquist <henri.sundquist@rokki.net>
2021-08-12 12:53:26 +0300

    Added __str__ to userprofile model

--- qleader/models/userprofile.py
+++ qleader/models/userprofile.py
@@ -22,3 +22,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+
+
+def __str__(self):
+    return f'UserProfile: {self.user}'
