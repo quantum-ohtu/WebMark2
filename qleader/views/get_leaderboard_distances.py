d527d0a0c8032c83bfbdabe53d2dd55a9920dd26
Petri Riihikallio <petri.riihikallio@metis.fi>
2021-08-12 22:24:58 +0300

    Add a blank line

--- qleader/views/get_leaderboard_distances.py
+++ qleader/views/get_leaderboard_distances.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import numpy as np
 
+
 @api_view(["GET"])
 def get_leaderboard_distances(request):
     if request.method == "GET":
